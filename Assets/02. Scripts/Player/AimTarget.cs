03522af917420bf72f9f5edaf9d2dbbfb3e55ef3
Sumin Cha <109404269+goliot@users.noreply.github.com>
2025-05-07 14:17:40 +0900

    250507 / 조준 방향으로 총구 따라가기 IK

---
+++ Assets/02. Scripts/Player/AimTarget.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class AimTarget : MonoBehaviour
+{
+    private Ray _ray;
+    private RaycastHit _hitInfo;
+
+    private Vector3 hitPoint;
+    private bool _isHit;
+
+    void LateUpdate()
+    {
+        _ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        _isHit = Physics.Raycast(_ray, out _hitInfo, Mathf.Infinity, ~(1 << LayerMask.NameToLayer("Player")));
+
+        if (_isHit)
+        {
+            hitPoint = _hitInfo.point;
+            transform.position = _hitInfo.point;
+        }
+        else
+        {
+            hitPoint = _ray.origin + _ray.direction * 50f;
+            transform.position = hitPoint;
+        }
+    }
+}
