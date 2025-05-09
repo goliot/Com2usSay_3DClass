f1dc0849978792daa9d11d430a9de0d09f2617ea
Sumin Cha <109404269+goliot@users.noreply.github.com>
2025-05-09 16:02:36 +0900

    250509 / 비밀번호 SHA256암호화, 소금 기법, 씬연결

--- Assets/02. Scripts/UI/Lobby/UI_LoginScene.cs
+++ Assets/02. Scripts/UI/Lobby/UI_LoginScene.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 [Serializable]
 public struct AccountInputFields
@@ -27,6 +28,14 @@ public struct AccountInputFields
             Fields.Add(PasswordCheck);
         }
     }
+
+    public void Clear()
+    {
+        foreach(var input in Fields)
+        {
+            input.text = string.Empty;
+        }
+    }
 }
 
 public class UI_LoginScene : MonoBehaviour
@@ -42,11 +51,17 @@ public class UI_LoginScene : MonoBehaviour
     [Header("# Register")]
     [SerializeField] private AccountInputFields _registerFields;
 
+    [Header("# Security")]
+    private const string PREFIX = "ID_";
+    private const string SALT = "10014001209";
+
     private AccountInputFields _nowPanel;
     private Vector3 _resultOriginPos;
 
-    private void Start()
+    private void Awake()
     {
+        //PlayerPrefs.DeleteAll();
+        //PlayerPrefs.Save();
         _loginFields.InitFields();
         _registerFields.InitFields();
         _loginPanel.SetActive(true);
@@ -54,6 +69,7 @@ public class UI_LoginScene : MonoBehaviour
         _nowPanel = _loginFields;
         _resultOriginPos = _resultText.rectTransform.position;
         SetResultText("");
+        InputCheck();
     }
 
     private void Update()
@@ -88,6 +104,8 @@ public class UI_LoginScene : MonoBehaviour
         _loginPanel.SetActive(true);
         _registerPanel.SetActive(false);
         _nowPanel = _loginFields;
+        _nowPanel.Clear();
+        InputCheck();
     }
 
     public void OnClickGoToRegisterButton()
@@ -95,6 +113,8 @@ public class UI_LoginScene : MonoBehaviour
         _loginPanel.SetActive(false);
         _registerPanel.SetActive(true);
         _nowPanel = _registerFields;
+        _nowPanel.Clear();
+        InputCheck();
     }
 
     public void Register()
@@ -102,58 +122,62 @@ public class UI_LoginScene : MonoBehaviour
         string id = _registerFields.IDInputField.text;
         string pw = _registerFields.PasswordInputField.text;
         string pwCheck = _registerFields.PasswordCheck.text;
+        string idWithPrefix = PREFIX + id;
 
         if (string.IsNullOrEmpty(id))
         {
-            SetResultText("아이디를 입력해주세요.");
+            SetResultText("아이디를 입력해주세요.", false);
             return;
         }
         else if (string.IsNullOrEmpty(pw) || string.IsNullOrEmpty(pwCheck))
         {   
-            SetResultText("비밀번호 또는 확인을 입력해주세요.");
+            SetResultText("비밀번호 또는 확인을 입력해주세요.", false);
             return;
         }
         else if(pw != pwCheck)
         {
-            SetResultText("입력하신 비밀번호와 확인이 일치하지 않습니다.");
+            SetResultText("입력하신 비밀번호와 확인이 일치하지 않습니다.", false);
             return;
         }
-        else if (PlayerPrefs.HasKey(id))
+        else if (PlayerPrefs.HasKey(idWithPrefix))
         {
-            SetResultText("이미 존재하는 아이디입니다.");
+            SetResultText("이미 존재하는 아이디입니다.", false);
             return;
         }
 
-        PlayerPrefs.SetString(id, AesEncryption.Encrypt(pw));
-        SetResultText("회원가입이 완료되었습니다.");
+        PlayerPrefs.SetString(idWithPrefix, SHA256Encryption.Encrypt(pw + SALT));
+        SetResultText("회원가입이 완료되었습니다.", true);
         OnClickGoToLoginButton();
         _loginFields.IDInputField.text = id;
-        _loginFields.PasswordInputField.text = "";
+        _loginFields.PasswordInputField.text = string.Empty;
     }
 
     public void Login()
     {
         string id = _loginFields.IDInputField.text;
         string pw = _loginFields.PasswordInputField.text;
+        string idWithPrefix = PREFIX + id;
+
         if (string.IsNullOrEmpty(id))
         {
-            SetResultText("아이디를 입력해주세요.");
+            SetResultText("아이디를 입력해주세요.", false);
             return;
         }
         else if (string.IsNullOrEmpty(pw))
         {
-            SetResultText("비밀번호를 입력해주세요.");
+            SetResultText("비밀번호를 입력해주세요.", false);
             return;
         }
-        else if(!PlayerPrefs.HasKey(id) || AesEncryption.Decrypt(PlayerPrefs.GetString(id, "")) != pw)
+        else if(!PlayerPrefs.HasKey(idWithPrefix) || PlayerPrefs.GetString(idWithPrefix, "") != SHA256Encryption.Encrypt(pw + SALT))
         {
-            SetResultText("아이디 또는 비밀번호가 일치하지 않거나 존재하지 않는 계정입니다.");
+            SetResultText("아이디 또는 비밀번호가 일치하지 않거나 존재하지 않는 계정입니다.", false);
             return;
         }
 
         //TODO : 로그인 성공, 씬 로드
-        SetResultText("로그인 성공!");
-        Debug.Log(PlayerPrefs.GetString(id));
+        SetResultText("로그인 성공!", true);
+        Debug.Log(PlayerPrefs.GetString(idWithPrefix));
+        SceneManager.LoadScene(1);
     }
 
     private void SetResultText(string s)
@@ -162,5 +186,30 @@ public class UI_LoginScene : MonoBehaviour
         _resultText.DOKill();
         _resultText.rectTransform.position = _resultOriginPos;
         _resultText.rectTransform.DOShakeAnchorPos(0.3f, 30, 100).OnComplete(() => _resultText.rectTransform.position = _resultOriginPos);
+    }
+
+    private void SetResultText(string s, bool isSuccess)
+    {
+        _resultText.text = s;
+        _resultText.color = isSuccess ? Color.green : Color.red;
+        _resultText.DOKill();
+        _resultText.rectTransform.position = _resultOriginPos;
+        if (!isSuccess)
+        {
+            _resultText.rectTransform.DOShakeAnchorPos(0.3f, 30, 100).OnComplete(() => _resultText.rectTransform.position = _resultOriginPos);
+        }
+        else
+        {
+            _resultText.rectTransform.DOShakeScale(0.3f);
+        }
+    }
+
+    public void InputCheck()
+    {
+        string id = _nowPanel.IDInputField.text;
+        string pw = _nowPanel.PasswordInputField.text;
+        string pwCheck = _nowPanel.PasswordCheck != null ? _nowPanel.PasswordCheck.text : null;
+
+        _nowPanel.ConfirmButton.interactable = !string.IsNullOrEmpty(id) && !string.IsNullOrEmpty(pw) && (pwCheck == null || !string.IsNullOrEmpty(pwCheck));
     }
 }
--- Assets/02. Scripts/UI/Popup/UI_TouchBounce.cs
+++ Assets/02. Scripts/UI/Popup/UI_TouchBounce.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class UI_TouchBounce : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
@@ -9,25 +10,39 @@ public class UI_TouchBounce : MonoBehaviour, IPointerDownHandler, IPointerUpHand
     public float Duration = 0.2f;
 
     private RectTransform _rectTransform;
+    private Button _button;
 
     private void Awake()
     {
         _rectTransform = GetComponent<RectTransform>();
         StartScale = transform.localScale.x;
+        _button = GetComponent<Button>();
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        if(!_button.interactable)
+        {
+            return;
+        }
         transform.DOScale(EndScale, Duration).SetEase(Ease.InOutBounce).OnComplete(() => transform.localScale = Vector3.one * EndScale).SetUpdate(true);
     }
 
     public virtual void OnPointerUp(PointerEventData eventData)
     {
+        if (!_button.interactable)
+        {
+            return;
+        }
         transform.DOScale(StartScale, Duration).SetEase(Ease.InOutBounce).OnComplete(() => transform.localScale = Vector3.one * StartScale).SetUpdate(true);
     }
 
     public void Bounce()
     {
+        if (!_button.interactable)
+        {
+            return;
+        }
         transform.DOKill(); // 이전 트윈 제거
         transform.localScale = Vector3.one * StartScale;
         transform.DOScale(EndScale, Duration)
